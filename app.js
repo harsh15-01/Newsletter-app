8eb08712fcb271edacf0239c9227270534657669
harsh15-01 <95934620+harsh15-01@users.noreply.github.com>
2023-04-28 19:42:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/f2bfabbeaf";
+
+  const options = {
+    method: "POST",
+    auth: "harsh15:e9971c825c368866208b237889a25fc7-us21"
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } 
+    else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
